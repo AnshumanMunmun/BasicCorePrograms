65cdddc45652b48898da00f3a78bbc27fb359c94
AnshumanMunmun <imanshuman47@gmail.com>
2022-08-25 20:31:59 +0530

    [Anshuman] Add: Adding Leap Year Program

---
+++ BasicCorePrograms/LeapYear.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class LeapYear
+    {
+        public void Read() //Reading input from user
+        {
+            Console.WriteLine("please enter the year : ");
+            string input = Console.ReadLine();
+            int year = Convert.ToInt32(input);    //Converts the value of string to an equivalent 32-bit signed int.
+            leapYear(year);                       //calling method leapYear
+        }
+        private static void leapYear(int year)    //Check the Conditions of LeapYear by using ForLoop
+        {                                         //if year is divisible by both 4 and 400 then its a LeapYear
+            if ((year % 4 == 0) && (year % 400 != 0) || (year % 4 == 0 && (year % 100 == 0)))
+            {
+                Console.WriteLine(year + " is a leap year");
+            }
+            else
+                Console.WriteLine(year + " is not a leap year");
+        }
+    }
+}
--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -6,6 +6,7 @@
         {
             Console.WriteLine("Choose the following programs");
             Console.WriteLine("1. FlipCoin ");
+            Console.WriteLine("2. LeapYear ");
             Console.WriteLine("0. Exit ");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
@@ -14,6 +15,11 @@
                     Console.WriteLine("1. Flip A Coin");
                     FlipCoin flipCoin = new FlipCoin();
                     flipCoin.Read();
+                    break;
+                case 2:
+                    Console.WriteLine("2. LeapYear ");
+                    LeapYear leapyear = new LeapYear();
+                    leapyear.Read();
                     break;
                 default:
                     break;
